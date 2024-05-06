786ecfd356573d8bda75a4c3a4f67f1b6a787ef6
Craig Smith <952595+phpsa@users.noreply.github.com>
2024-05-06 15:33:37 +1200

    chore: added new db and redis container for whatever reason.
    tinker run config('database') - shows all database types along with my new v2, redis only has my new v2 and all other keys are missing

---
+++ config/database.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+
+
+    'connections' => [
+
+        'sqlite_v2' => [
+            'driver' => 'sqlite',
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
+
+
+    ],
+
+    'redis' => [
+
+
+        'cache_v2' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+    ],
+
+];
